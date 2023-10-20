cc1bd3a8aafec8b803b33a83eb277a20fac18230
JoanaF <joanadiasf2001@gmail.com>
2023-10-20 15:59:46 +0100

    ex 3 e apontamentos de ex4

--- FichaJava/src/FichaPratica02/Ex_03.java
+++ FichaJava/src/FichaPratica02/Ex_03.java
@@ -1,4 +1,35 @@
 package FichaPratica02;
 
+import java.util.Scanner;
 public class Ex_03 {
+
+    public static void main(String[] args) {
+
+        // Instanciar o Scanner obrigatório para todos os exercicios)
+        Scanner input = new Scanner(System.in);
+
+        //Declarar variaveis
+        double salario;
+
+        //Ler valores
+        System.out.println("Salário anual: ");
+        salario = input.nextDouble();
+
+        //Condicional
+        if (salario <= 15000){
+            System.out.println("Taxa a pagar - 20% : " + salario * 0.2);
+        }
+
+        if (salario > 15000 && salario <= 20000){
+            System.out.println("Taxa a pagar - 30% : " + salario * 0.3);
+        }
+
+        if (salario > 20000 && salario <= 25000){
+            System.out.println("Taxa a pagar - 35% :" + salario * 0.35);
+        }
+
+        if (salario > 25000){
+            System.out.println("Taxa a pagar - 40% :" + salario * 0.4);
+        }
+    }
 }
--- FichaJava/src/FichaPratica02/Ex_04.java
+++ FichaJava/src/FichaPratica02/Ex_04.java
@@ -1,4 +1,33 @@
 package FichaPratica02;
 
+import java.util.Scanner;
+
 public class Ex_04 {
+
+    public static void main(String[] args) {
+
+
+    }
 }
+
+//declarar variáveis - lugar
+
+//switch (lugar){
+//case 1:
+//sout
+//break;
+
+//case 2:
+//sout
+//break;
+
+// case 3:
+// sout
+// break;
+
+//case 8 :
+//sout
+//break;
+
+//default:
+//sout
