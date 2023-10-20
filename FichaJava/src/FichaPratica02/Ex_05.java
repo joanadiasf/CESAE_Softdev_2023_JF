b6b97f46f154018bb9c400f92cf470d4b93ef969
JoanaF <joanadiasf2001@gmail.com>
2023-10-20 16:21:15 +0100

    ex5

--- FichaJava/src/FichaPratica02/Ex_05.java
+++ FichaJava/src/FichaPratica02/Ex_05.java
@@ -1,4 +1,33 @@
 package FichaPratica02;
 
+import java.util.Scanner;
 public class Ex_05 {
+    public static void main(String[] args) {
+
+        // Instanciar o Scanner obrigatório para todos os exercicios)
+        Scanner input = new Scanner(System.in);
+
+        //Declarar variaveis
+        double num1,num2;
+
+        //Ler variaveis
+        System.out.println("Introduzir 1º número: ");
+        num1= input.nextDouble();
+
+        System.out.println("Introduzir 2º número: ");
+        num2= input.nextDouble();
+
+        //Condicionais
+        if (num1>num2){
+            System.out.println("\nMenor: " + num2 + "       Maior:" + num1);
+        }
+
+        if (num2>num1){
+            System.out.println("\nMenor: " +num1 + "       Maior:" + num2);
+        }
+
+        if (num1==num2){
+            System.out.println("\nOs números são iguais");
+        }
+    }
 }
