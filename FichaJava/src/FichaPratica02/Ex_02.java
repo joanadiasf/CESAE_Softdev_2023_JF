015ecca93cb8a1ea0e2ce07b041b18205763e21b
JoanaF <joanadiasf2001@gmail.com>
2023-10-20 11:04:24 +0100

    ex02

--- FichaJava/src/FichaPratica02/Ex_02.java
+++ FichaJava/src/FichaPratica02/Ex_02.java
@@ -1,4 +1,27 @@
 package FichaPratica02;
 
+import java.util.Scanner;
 public class Ex_02 {
+
+    public static void main(String[] args) {
+
+        // Instanciar o Scanner obrigatório para todos os exercicios)
+        Scanner input = new Scanner(System.in);
+
+        //Declarar variaveis
+        double salario;
+
+        //Ler valores
+        System.out.println("Salário anual: ");
+        salario = input.nextDouble();
+
+        //Condicional
+
+        if (salario < 15000)
+            System.out.println("Taxa a pagar: " + salario * 0.2);
+
+        else
+            System.out.println("Taxa a pagar: " + salario * 0.3);
+
+    }
 }
