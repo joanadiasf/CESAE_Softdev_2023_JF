071fb766e5fbbd1071eb4400dd353d20382e7aac
JoanaF <joanadiasf2001@gmail.com>
2023-10-20 16:33:51 +0100

    ex7

--- FichaJava/src/FichaPratica02/Ex_07.java
+++ FichaJava/src/FichaPratica02/Ex_07.java
@@ -1,4 +1,27 @@
 package FichaPratica02;
 
+import java.util.Scanner;
 public class Ex_07 {
+
+    public static void main(String[] args) {
+
+        // Instanciar o Scanner obrigatório para todos os exercicios)
+        Scanner input = new Scanner(System.in);
+
+        //Declarar variaveis
+        double num;
+
+        //Ler variavel
+        System.out.println("Introduza um número: ");
+        num= input.nextDouble();
+
+        //Condicionais
+        if (num%2==0)
+            System.out.println("O número é par!");
+
+        else
+            System.out.println("O número é impar!");
+
+
+    }
 }
