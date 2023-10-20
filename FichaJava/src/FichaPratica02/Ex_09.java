8d5911750ecf1ea0aa5c791fce9a140cb4b6a1ed
JoanaF <joanadiasf2001@gmail.com>
2023-10-20 16:47:44 +0100

    ex9 inicio

--- FichaJava/src/FichaPratica02/Ex_09.java
+++ FichaJava/src/FichaPratica02/Ex_09.java
@@ -1,4 +1,16 @@
 package FichaPratica02;
 
+import java.util.Scanner;
 public class Ex_09 {
+
+    public static void main(String[] args) {
+
+
+        // Instanciar o Scanner obrigatório para todos os exercicios)
+        Scanner input = new Scanner(System.in);
+
+        //Declarar variaveis
+        int num1,num2,num3;
+        
+    }
 }
