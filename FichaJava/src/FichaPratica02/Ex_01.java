b3e1f5d7989358cc5567945bcfd8c3d7e65c1cf5
JoanaF <joanadiasf2001@gmail.com>
2023-10-20 10:55:46 +0100

    ex01

--- FichaJava/src/FichaPratica02/Ex_01.java
+++ FichaJava/src/FichaPratica02/Ex_01.java
@@ -1,7 +1,30 @@
 package FichaPratica02;
 
+import java.util.Scanner;
 public class Ex_01 {
 
+    public static void main(String[] args) {
+
+        // Instanciar o Scanner obrigatório para todos os exercicios)
+        Scanner input = new Scanner(System.in);
+
+        //Declarar variaveis
+        int valor1,valor2;
+
+        //Ler valores
+        System.out.println("Inserir valor: ");
+        valor1 = input.nextInt();
+
+        System.out.println("Inserir o outro valor: ");
+        valor2 = input.nextInt();
+
+        //Condicional
+
+        if (valor1 > valor2)
+            System.out.println("O valor maior é: " + valor1);
+        else
+            System.out.println("O valor maior é: " + valor2);
+    }
 
 
 }
