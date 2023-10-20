f6b16bb2c93ba01be24e12119227428ad40100a9
JoanaF <joanadiasf2001@gmail.com>
2023-10-20 16:45:19 +0100

    ex8

--- FichaJava/src/FichaPratica02/Ex_08.java
+++ FichaJava/src/FichaPratica02/Ex_08.java
@@ -1,4 +1,28 @@
 package FichaPratica02;
 
+import java.util.Scanner;
 public class Ex_08 {
+
+    public static void main(String[] args) {
+
+        // Instanciar o Scanner obrigatório para todos os exercicios)
+        Scanner input = new Scanner(System.in);
+
+        //Declarar variaveis
+        double nota1,nota2,nota3,mediap;
+
+        //Ler variaveis
+        System.out.println("Introduzir 1º nota: ");
+        nota1= input.nextDouble();
+
+        System.out.println("Introduzir 2º nota: ");
+        nota2= input.nextDouble();
+
+        System.out.println("Introduzir 3º nota: ");
+        nota3= input.nextDouble();
+
+        //Calcular a media
+        mediap=(nota1*0.25) + (nota2*0.35) + (nota3*0.4);
+        System.out.println(" A média final ponderada é: " + mediap);
+    }
 }
