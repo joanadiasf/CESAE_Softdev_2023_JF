965a07b9d4ca253088eb80d9f9ad1b9d64b80eaa
JoanaF <joanadiasf2001@gmail.com>
2023-10-20 09:17:55 +0100

    ex05

--- FichaJava/src/FichaPratica01/Ex_05.java
+++ FichaJava/src/FichaPratica01/Ex_05.java
@@ -5,7 +5,30 @@ public class Ex_05 {
 
     public static void main(String[] args) {
 
+        // Instanciar o Scanner obrigatório para todos os exercicios)
+        Scanner input = new Scanner(System.in);
+
+        //Declarar variaveis
+        double valor1,valor2,valor3,mediaar,mediapon;
+
+        //Ler valores
+        System.out.println("Insira um valor");
+        valor1 = input.nextInt();
+
+        System.out.println("Insira um valor");
+        valor2 = input.nextInt();
+
+        System.out.println("Insira um valor");
+        valor3 = input.nextInt();
+
+        //Calcular e apresentar medias
+        mediaar = (valor1 + valor2 + valor3) / 3;
+
+        mediapon = ( (valor1 * 0.5) + (valor2 * 0.3) + (valor3 * 0.2) );
 
 
+        System.out.println("Média Aritmética é: " + mediaar);
+        System.out.println("Média Ponderada é: " + mediapon);
+        
     }
 }
