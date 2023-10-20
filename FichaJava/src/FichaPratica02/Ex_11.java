8b0e174b28bf4ea68b8310263bf14b576f6faa49
siriusjnr <139019457+siriusjnr@users.noreply.github.com>
2023-10-20 23:28:34 +0100

    start ex11

--- FichaJava/src/FichaPratica02/Ex_11.java
+++ FichaJava/src/FichaPratica02/Ex_11.java
@@ -1,4 +1,18 @@
 package FichaPratica02;
 
+import java.util.Scanner;
+
 public class Ex_11 {
+
+    public static void main(String[] args) {
+
+        // Instanciar o Scanner obrigatório para todos os exercicios)
+        Scanner input = new Scanner(System.in);
+
+        //Declarar variaveis
+
+        //Ler / input
+
+        //if else
+    }
 }
