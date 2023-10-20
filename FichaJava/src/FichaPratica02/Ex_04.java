18cdab99acdb1065c85db84a7dc9f6645053d073
JoanaF <joanadiasf2001@gmail.com>
2023-10-20 16:12:37 +0100

    ex4
    switch

--- FichaJava/src/FichaPratica02/Ex_04.java
+++ FichaJava/src/FichaPratica02/Ex_04.java
@@ -6,28 +6,61 @@ public class Ex_04 {
 
     public static void main(String[] args) {
 
+        // Instanciar o Scanner obrigatório para todos os exercicios)
+        Scanner input = new Scanner(System.in);
+
+        //Declarar variaveis
+        int lugar;
+
+        //Ler lugar
+        System.out.println("Em que lugar ficou? ");
+        lugar = input.nextInt();
+
+        //Condicional
+        switch (lugar){
+            case 1:
+                System.out.println("Ganhou 10 pontos!");
+                break;
+
+            case 2:
+                System.out.println("Ganhou 8 pontos!");
+                break;
+
+            case 3:
+                System.out.println("Ganhou 6 pontos!");
+                break;
+
+            case 4:
+                System.out.println("Ganhou 5 pontos!");
+                break;
+
+            case 5:
+                System.out.println("Ganhou 4 pontos!");
+                break;
+
+            case 6:
+                System.out.println("Ganhou 3 pontos!");
+                break;
+
+            case 7:
+                System.out.println("Ganhou 2 pontos!");
+                break;
+
+            case 8:
+                System.out.println("Ganhou 1 pontos!");
+                break;
+
+            default:
+                System.out.println("Não ganhou pontos...");
+        }
 
     }
 }
 
-//declarar variáveis - lugar
 
-//switch (lugar){
-//case 1:
-//sout
-//break;
 
-//case 2:
-//sout
-//break;
 
-// case 3:
-// sout
-// break;
 
-//case 8 :
-//sout
-//break;
 
 //default:
 //sout
