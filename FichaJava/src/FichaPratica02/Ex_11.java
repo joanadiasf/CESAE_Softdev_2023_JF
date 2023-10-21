f3fec5c4d42f85a095852e70a728a550fe6f37f6
siriusjnr <joanadiasf2001@gmail.com>
2023-10-21 11:47:19 +0100

    ex11

--- FichaJava/src/FichaPratica02/Ex_11.java
+++ FichaJava/src/FichaPratica02/Ex_11.java
@@ -10,9 +10,26 @@ public class Ex_11 {
         Scanner input = new Scanner(System.in);
 
         //Declarar variaveis
+        double num1,num2,creditar,debitar;
 
         //Ler / input
+        System.out.println("Saldo da conta bancária: ");
+        num1 = input.nextDouble();
+
+        System.out.println("Montante a creditar/debitar: ");
+        num2 = input.nextDouble();
 
         //if else
+
+        debitar = num1 - num2;
+        creditar = num1 + num2;
+
+        if (debitar >= 0 && creditar >=0){
+            System.out.println("Saldo atual: " + creditar);
+        }
+
+        else {
+            System.out.println("Saldo atual negativo!");
+        }
     }
 }
