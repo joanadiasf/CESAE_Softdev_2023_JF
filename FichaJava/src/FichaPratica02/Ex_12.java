4815aa0720a5fc072f9e9eb29f9dc165e24ae96c
siriusjnr <joanadiasf2001@gmail.com>
2023-10-21 11:57:35 +0100

    ex12

--- FichaJava/src/FichaPratica02/Ex_12.java
+++ FichaJava/src/FichaPratica02/Ex_12.java
@@ -1,4 +1,40 @@
 package FichaPratica02;
 
+import java.util.Scanner;
 public class Ex_12 {
+
+    public static void main(String[] args) {
+
+        // Instanciar o Scanner obrigatório para todos os exercicios)
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        int escolha;
+
+        //Condicional
+        System.out.println("Menu: \n 1.Criar 2.Atualizar 3.Eliminar 4.Sair");
+        escolha = input.nextInt();
+
+        switch (escolha){
+
+            case 1:
+                System.out.println("Selecionou a opção 1. Criar.");
+                break;
+
+            case 2:
+                System.out.println("Selecionou a opção 2. Atualizar.");
+                break;
+
+            case 3:
+                System.out.println("Selecionou a opção 3.Eliminar.");
+                
+            case 4:
+                System.out.println("          ");
+                break;
+
+            default:
+                System.out.println("Opção inválida");
+
+        }
+    }
 }
