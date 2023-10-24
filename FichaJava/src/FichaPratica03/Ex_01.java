2896bf727dd8ea150a799f60bec6d9301a73e3ee
siriusjnr <joanadiasf2001@gmail.com>
2023-10-24 16:50:37 +0100

    ficha3

---
+++ FichaJava/src/FichaPratica03/Ex_01.java
@@ -0,0 +1,23 @@
+package FichaPratica03;
+
+import java.util.Scanner;
+
+public class Ex_01 {
+
+    public static void main(String[] args) {
+
+        //Declarar variaveis
+        int num;
+
+
+        num=1;
+
+
+        //Loop
+
+        while (num <=250){
+            System.out.println(num);
+            num=num+1;
+        }
+    }
+}
---
+++ FichaJava/src/FichaPratica03/Ex_02.java
@@ -0,0 +1,24 @@
+package FichaPratica03;
+
+import java.util.Scanner;
+
+public class Ex_02 {
+
+    public static void main(String[] args) {
+
+        //Declarar variáveis
+        int num;
+
+        //Ler variavel
+        num = 2;
+
+        //Loop
+
+        while (num >= 2 && num <=400){
+
+            System.out.println(num);
+            num=num+2;
+        }
+    }
+}
+ 
