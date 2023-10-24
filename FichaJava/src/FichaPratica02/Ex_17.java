146387740b43a0186d5ce8bdd4a46abfc6e51e30
siriusjnr <joanadiasf2001@gmail.com>
2023-10-24 09:26:40 +0100

    ex17

--- FichaJava/src/FichaPratica02/Ex_17.java
+++ FichaJava/src/FichaPratica02/Ex_17.java
@@ -1,4 +1,40 @@
 package FichaPratica02;
 
+import java.util.Scanner;
 public class Ex_17 {
+
+    public static void main(String[] args) {
+
+        // Instanciar o Scanner obrigatório para todos os exercicios)
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        double valor,credito;
+
+        //Ler valor
+        System.out.println("Insira o seu salário médio: ");
+        valor = input.nextDouble();
+
+        //Condicionais
+
+        if (valor > 0 && valor <= 2000){
+            credito = valor;
+            System.out.println("Saldo médio: " + valor + "Crédito: Nenhum crédito");
+        }
+
+        if (valor > 2000 && valor <= 4000){
+            credito = valor * 0.2;
+            System.out.println("Saldo médio: " + valor + "Crédito: " + credito);
+        }
+
+        if (valor > 4000 && valor <= 6000){
+            credito = valor * 0.3;
+            System.out.println("Saldo médio: " + valor + "Crédito: " + credito);
+        }
+
+        if (valor > 6000){
+            credito = valor * 0.4;
+            System.out.println("Saldo médio: " + valor + "Crédito: " + credito);
+        }
+    }
 }
