2c1ad285af4a494216ffe1272a43553849a51039
siriusjnr <joanadiasf2001@gmail.com>
2023-10-24 15:04:40 +0100

    EX5

--- FichaJava/src/FIchaPraticaExtraCondicionais/Ex_04.java
+++ FichaJava/src/FIchaPraticaExtraCondicionais/Ex_04.java
@@ -24,6 +24,6 @@ public class Ex_04 {
         }
 
     }
-    
+
 }
 
--- FichaJava/src/FIchaPraticaExtraCondicionais/Ex_05.java
+++ FichaJava/src/FIchaPraticaExtraCondicionais/Ex_05.java
@@ -1,4 +1,49 @@
 package FIchaPraticaExtraCondicionais;
 
+import java.util.Scanner;
+
 public class Ex_05 {
+
+    public static void main(String[] args) {
+
+        // Instanciar o Scanner obrigatório para todos os exercicios)
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        int num, tentiva;
+
+        //Ler variaveis
+        num = 74;
+
+        System.out.println(" Adivinhe o número de 1 a 100! ( 2 tentativas)");
+        tentiva = input.nextInt();
+
+        //Condicional
+
+        if (tentiva <= 73 && tentiva > 1){
+
+                if (tentiva < 55){
+                    System.out.println("Muito baixo...");
+                } else {
+                    System.out.println("Baixo, mas perto.");
+                }
+        }
+
+
+        if (tentiva < 100 && tentiva > 73 && tentiva !=74){
+
+            if (tentiva > 85){
+                System.out.println("Muito alto");
+            } else {
+                System.out.println("Alto, mas perto.");
+            }
+        }
+
+
+        if (tentiva == num){
+            System.out.println("Parabéns! Acertou.");
+        }
+
+
+    }
 }
