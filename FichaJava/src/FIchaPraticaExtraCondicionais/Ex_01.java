6ff392b6a08d9b9d761dfdc690423bc37f60f3a7
siriusjnr <joanadiasf2001@gmail.com>
2023-10-24 14:23:59 +0100

    ex1 e 2

--- FichaJava/src/FIchaPraticaExtraCondicionais/Ex_01.java
+++ FichaJava/src/FIchaPraticaExtraCondicionais/Ex_01.java
@@ -1,4 +1,27 @@
 package FIchaPraticaExtraCondicionais;
 
+import java.util.Scanner;
+
 public class Ex_01 {
+
+    public static void main(String[] args) {
+
+        // Instanciar o Scanner obrigatório para todos os exercicios)
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        int idade;
+
+        //Ler idade
+        System.out.println("Insira a sua idade: \n");
+        idade = input.nextInt();
+
+        //Condicionais
+        if (idade >=18){
+            System.out.println("Maior de idade");
+        }
+            else {
+            System.out.println("Menor de idade");
+        }
+    }
 }
--- FichaJava/src/FIchaPraticaExtraCondicionais/Ex_02.java
+++ FichaJava/src/FIchaPraticaExtraCondicionais/Ex_02.java
@@ -1,4 +1,25 @@
 package FIchaPraticaExtraCondicionais;
 
+import java.util.Scanner;
 public class Ex_02 {
+
+    public static void main(String[] args) {
+
+        // Instanciar o Scanner obrigatório para todos os exercicios)
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        double peso;
+
+        //Ler peso
+        System.out.println("Insira o seu peso(em KG): \n");
+        peso = input.nextDouble();
+
+        if (peso > 0){
+            System.out.println("O seu peso na lua é: \n" + (peso*0.16));
+        }
+            else {
+            System.out.println("*| Peso inválido... | *");
+        }
+    }
 }
