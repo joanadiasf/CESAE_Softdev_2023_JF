2656353abe05ddd2086b21e78a97739958f1259b
siriusjnr <joanadiasf2001@gmail.com>
2023-10-24 10:52:26 +0100

    ex

--- FichaJava/src/FichaPratica02/Ex_16.java
+++ FichaJava/src/FichaPratica02/Ex_16.java
@@ -1,4 +1,53 @@
 package FichaPratica02;
 
+import java.util.Scanner;
+
 public class Ex_16 {
+
+    public static void main(String[] args) {
+
+        // Instanciar o Scanner obrigatório para todos os exercicios)
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        int dinheiro;
+
+        //Ler variaveis
+        System.out.println("Insira um valor (que seja múltiplo de 5) para saber as notas necessárias: \n");
+        dinheiro = input.nextInt();
+
+        //Condicionais
+        if (dinheiro / 5 == 1){
+            System.out.println("1 nota de 5€");
+        }
+
+        if (dinheiro / 5 == 2){
+            System.out.println("1 nota de 10€");
+        }
+
+        if (dinheiro / 5 == 3){
+            System.out.println("1 nota de 5€ e 1 nota de 10€");
+        }
+
+        if ( dinheiro / 5 == 4){
+            System.out.println("1 nota de 20€");
+        }
+
+        if (dinheiro / 5 == 5){
+            System.out.println("1 nota de 5€ e 1 nota de 20€");
+        }
+
+        if ( dinheiro / 5 == 6){
+            System.out.println("1 nota de 20€ e 1 nota de 10€");
+        }
+
+        if ( dinheiro / 5 == 7){
+            System.out.println("1 nota de 20€, 1 nota de 10€ e 1 nota de 5€");
+        }
+
+        if ( dinheiro / 5 == 8){
+            System.out.println("2 nota de 20€");
+        }
+
+    }
 }
--- FichaJava/src/FichaPratica02/Ex_18.java
+++ FichaJava/src/FichaPratica02/Ex_18.java
@@ -10,80 +10,81 @@ public class Ex_18 {
         Scanner input = new Scanner(System.in);
 
         // Declarar variáveis
-        double codfun,cargo,diastrab,iliquido,subali,irs,ss1,ss2,liquido;
+        double cargo,iliquido,subali,irs,ss1,ss2,liquido;
+        int codfun,diastrab;
 
         //ler info
         System.out.println("Insirir código de funcionário");
-        codfun = input.nextDouble();
+        codfun = input.nextInt();
 
         System.out.println("Dias trabalhados");
-        diastrab = input.nextDouble();
+        diastrab = input.nextInt();
 
         System.out.println("Selecione cargo:     E-Empregado     C-Chefe     A-Administrador");
         cargo = input.nextDouble();
 
 
 
-//        switch (cargo){
-//
-//            case "E":
-//                subali= diastrab * 5;
-//                iliquido= (40 * diastrab);
-//                irs= (iliquido * 0.1);
-//                ss1= (iliquido * 0.11);
-//                ss2= (iliquido * 0.2375);
-//                liquido= ((iliquido + subali) - irs) - ss1 ;
-//
-//                System.out.println("Funcionário(a)" + codfun);
-//                System.out.println("Cargo: " + cargo);
-//                System.out.println("Valor ilíquido: " + iliquido);
-//                System.out.println("Subsídio de alimentação: " + subali);
-//                System.out.println("IRS: " + irs);
-//                System.out.println("A pagar à Segurança Social (funcionário): " + ss1);
-//                System.out.println("A pagar à Segurança Social (empresa): " + ss2);
-//                System.out.println("Valor líquido: " + liquido);
-//                break;
-//
-//            case "C":
-//                subali= diastrab * 7.5;
-//                iliquido= (60 * diastrab);
-//                irs= (iliquido * 0.2);
-//                ss1= (iliquido * 0.11);
-//                ss2= (iliquido * 0.2375);
-//                liquido= ((iliquido + subali) - irs) - ss1 ;
-//
-//                System.out.println("Funcionário(a)" + codfun);
-//                System.out.println("Cargo: " + cargo);
-//                System.out.println("Valor ilíquido: " + iliquido);
-//                System.out.println("Subsídio de alimentação: " + subali);
-//                System.out.println("IRS: " + irs);
-//                System.out.println("A pagar à Segurança Social (funcionário): " + ss1);
-//                System.out.println("A pagar à Segurança Social (empresa): " + ss2);
-//                System.out.println("Valor líquido: " + liquido);
-//                break;
-//
-//            case "A":
-//                subali= diastrab * 7.5;
-//                iliquido= (80 * diastrab);
-//                irs= (iliquido * 0.2);
-//                ss1= (iliquido * 0.11);
-//                ss2= (iliquido * 0.2375);
-//                liquido= ((iliquido + subali) - irs) - ss1 ;
-//
-//                System.out.println("Funcionário(a)" + codfun);
-//                System.out.println("Cargo: " + cargo);
-//                System.out.println("Valor ilíquido: " + iliquido);
-//                System.out.println("Subsídio de alimentação: " + subali);
-//                System.out.println("IRS: " + irs);
-//                System.out.println("A pagar à Segurança Social (funcionário): " + ss1);
-//                System.out.println("A pagar à Segurança Social (empresa): " + ss2);
-//                System.out.println("Valor líquido: " + liquido);
-//                break;
-//
-//            default:
-//                System.out.println("Cargo inválido");
-//
-//        }
+        switch (cargo){
+
+            case "E":
+                subali= diastrab * 5;
+                iliquido= (40 * diastrab);
+                irs= (iliquido * 0.1);
+                ss1= (iliquido * 0.11);
+                ss2= (iliquido * 0.2375);
+                liquido= ((iliquido + subali) - irs) - ss1 ;
+
+                System.out.println("Funcionário(a)" + codfun);
+                System.out.println("Cargo: " + cargo);
+                System.out.println("Valor ilíquido: " + iliquido);
+                System.out.println("Subsídio de alimentação: " + subali);
+                System.out.println("IRS: " + irs);
+                System.out.println("A pagar à Segurança Social (funcionário): " + ss1);
+                System.out.println("A pagar à Segurança Social (empresa): " + ss2);
+                System.out.println("Valor líquido: " + liquido);
+                break;
+
+            case "C":
+                subali= diastrab * 7.5;
+                iliquido= (60 * diastrab);
+                irs= (iliquido * 0.2);
+                ss1= (iliquido * 0.11);
+                ss2= (iliquido * 0.2375);
+                liquido= ((iliquido + subali) - irs) - ss1 ;
+
+                System.out.println("Funcionário(a)" + codfun);
+                System.out.println("Cargo: " + cargo);
+                System.out.println("Valor ilíquido: " + iliquido);
+                System.out.println("Subsídio de alimentação: " + subali);
+                System.out.println("IRS: " + irs);
+                System.out.println("A pagar à Segurança Social (funcionário): " + ss1);
+                System.out.println("A pagar à Segurança Social (empresa): " + ss2);
+                System.out.println("Valor líquido: " + liquido);
+                break;
+
+            case "A":
+                subali= diastrab * 7.5;
+                iliquido= (80 * diastrab);
+                irs= (iliquido * 0.2);
+                ss1= (iliquido * 0.11);
+                ss2= (iliquido * 0.2375);
+                liquido= ((iliquido + subali) - irs) - ss1 ;
+
+                System.out.println("Funcionário(a)" + codfun);
+                System.out.println("Cargo: " + cargo);
+                System.out.println("Valor ilíquido: " + iliquido);
+                System.out.println("Subsídio de alimentação: " + subali);
+                System.out.println("IRS: " + irs);
+                System.out.println("A pagar à Segurança Social (funcionário): " + ss1);
+                System.out.println("A pagar à Segurança Social (empresa): " + ss2);
+                System.out.println("Valor líquido: " + liquido);
+                break;
+
+            default:
+                System.out.println("Cargo inválido");
+
+        }
 //        //Calculos
 //        subali= diastrab * 5;
 //        iliquido= (40 * diastrab);
