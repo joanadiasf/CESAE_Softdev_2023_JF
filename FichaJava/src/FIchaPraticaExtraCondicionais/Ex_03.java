72df750e9d41fb353fdd2ade8091477b4c700660
siriusjnr <joanadiasf2001@gmail.com>
2023-10-24 14:32:34 +0100

    ex3

--- FichaJava/src/FIchaPraticaExtraCondicionais/Ex_03.java
+++ FichaJava/src/FIchaPraticaExtraCondicionais/Ex_03.java
@@ -1,4 +1,24 @@
 package FIchaPraticaExtraCondicionais;
 
+import java.util.Scanner;
 public class Ex_03 {
+    public static void main(String[] args) {
+        // Instanciar o Scanner obrigatório para todos os exercicios)
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        double preco;
+
+        //Ler preço
+        System.out.println("Insira o preço" );
+        preco = input.nextDouble();
+
+        //Condicionais
+        if (preco >= 100){
+            System.out.println("Preço final: " + (preco * 0.1) + "(10% de Desconto");
+        }
+            else {
+            System.out.println("Preço final: " + preco + "(Sem desconto)");
+        }
+    }
 }
