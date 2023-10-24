5511d8288fc3a433c37b7c48765a8f8c8f169728
siriusjnr <joanadiasf2001@gmail.com>
2023-10-24 14:42:59 +0100

    ex4

--- FichaJava/src/FIchaPraticaExtraCondicionais/Ex_04.java
+++ FichaJava/src/FIchaPraticaExtraCondicionais/Ex_04.java
@@ -3,13 +3,27 @@ package FIchaPraticaExtraCondicionais;
 import java.util.Scanner;
 
 public class Ex_04 {
-    
+
     public static void main(String[] args) {
 
         // Instanciar o Scanner obrigatório para todos os exercicios)
         Scanner input = new Scanner(System.in);
 
         // Declarar variáveis
+        String letra;
+
+        //Ler variavel
+        System.out.println("Insira uma letra (em minúsculas");
+        letra = input.next();
+
+        //Condicional
+        if (letra.equals("a") || letra.equals("e") || letra.equals("i") || letra.equals("o")|| letra.equals("u")){
+            System.out.println("A letra escolhida é uma VOGAL.");
+        } else {
+            System.out.println("A letra escolhida é uma CONSOANTE.");
+        }
+
     }
+    
 }
 
