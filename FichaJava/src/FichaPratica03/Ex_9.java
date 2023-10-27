d30ce284a9019e2a58f75382ab747e8bcd61280a
JoanaF <joanadiasf2001@gmail.com>
2023-10-27 10:12:55 +0100

    ex9

---
+++ FichaJava/src/FichaPratica03/Ex_9.java
@@ -0,0 +1,29 @@
+package FichaPratica03;
+
+import java.util.Scanner;
+public class Ex_9 {
+
+    public static void main(String[] args) {
+
+        // Instanciar o Scanner obligatorio para todos os exercicios)
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        int num,dois;
+
+        //Ler variáveis
+        dois = 2;
+
+        System.out.println("Insira um número inteiro: ");
+        num = input.nextInt();
+
+
+        while (num >= dois){
+            System.out.println(dois);
+
+            dois= dois +2;
+        }
+
+
+    }
+}
