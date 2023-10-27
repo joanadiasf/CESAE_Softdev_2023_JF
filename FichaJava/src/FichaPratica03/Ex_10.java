3ed9ffbcaf5242f1e9785d15b3c8b29f0cca7eec
JoanaF <joanadiasf2001@gmail.com>
2023-10-27 10:40:09 +0100

    ex10

--- FichaJava/src/FichaPratica03/Ex_10.java
+++ FichaJava/src/FichaPratica03/Ex_10.java
@@ -1,4 +1,32 @@
 package FichaPratica03;
 
+import java.util.Scanner;
 public class Ex_10 {
+
+    public static void main(String[] args) {
+
+        // Instanciar o Scanner obligatorio para todos os exercicios)
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        int limite, soma,salto;
+
+        //Ler variáveis
+        System.out.println("Introduza um limite: ");
+        limite = input.nextInt();
+
+        System.out.println("Introduza um salto: ");
+        salto = input.nextInt();
+        soma=salto;
+
+        System.out.println("0");
+        System.out.println(salto);
+
+        while (limite > soma){
+            System.out.println(soma);
+            soma= salto+soma;
+
+
+        }
+    }
 }
