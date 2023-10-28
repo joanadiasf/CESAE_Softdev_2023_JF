306cd89e1955ebd320135812f0379b500c637167
siriusjnr <joanadiasf2001@gmail.com>
2023-10-28 16:32:00 +0100

    AAAAAH14

--- FichaJava/src/FichaPratica03/Ex_14.java
+++ FichaJava/src/FichaPratica03/Ex_14.java
@@ -9,18 +9,31 @@ public class Ex_14 {
         Scanner input = new Scanner(System.in);
 
         // Declarar variáveis
-        int n,cont;
+        int n,aux,total;
 
         //Ler variáveis
         System.out.println("Insira um número inteiro não-negativo: ");
         n = input.nextInt();
-        cont=1;
 
+        total= n;
+        aux=n;
         //cont soma ate valor
         //fatorial valor dado x cont
+        //5x4x3x2x1=120
+
 
         //Loop
-        
+        while ( aux > 0 ){
 
+            aux = n-1;
+            if (n >= aux){
+
+                total=total*aux;
+
+            }
+
+
+        }
+        System.out.println(total);
     }
 }
