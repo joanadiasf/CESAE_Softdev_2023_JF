de04896458e242e639ca209f7a85cfad47a8843b
siriusjnr <joanadiasf2001@gmail.com>
2023-10-28 17:20:30 +0100

    ex1

--- FichaJava/src/FichaPraticaExtraCiclos/Ex_01.java
+++ FichaJava/src/FichaPraticaExtraCiclos/Ex_01.java
@@ -1,4 +1,15 @@
 package FichaPraticaExtraCiclos;
 
+import java.util.Scanner;
+
 public class Ex_01 {
+
+    public static void main(String[] args) {
+
+        // Instanciar o Scanner obligatorio para todos os exercicios)
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+    }
+
 }
