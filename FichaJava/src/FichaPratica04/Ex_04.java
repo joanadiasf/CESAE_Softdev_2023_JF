0ae1a8705d7e913a9760ea5c20b2bcbfda020dcf
siriusjnr <joanadiasf2001@gmail.com>
2023-10-30 20:22:07 +0000

    Update Ex_04.java

--- FichaJava/src/FichaPratica04/Ex_04.java
+++ FichaJava/src/FichaPratica04/Ex_04.java
@@ -1,4 +1,20 @@
 package FichaPratica04;
 
+import java.util.Scanner;
 public class Ex_04 {
+
+    public static void main(String[] args) {
+
+        //Scanner
+
+        Scanner input = new Scanner(System.in);
+
+        //Declarar + ler variaveis
+        int valor;
+
+        System.out.println("Insira um número para ver se este é primo: \n");
+        valor = input.nextInt();
+
+
+    }
 }
