8887786122b6b9402ca4fc1fa02982c8ea7bb81d
JoanaF <joanadiasf2001@gmail.com>
2023-10-30 15:00:27 +0000

    Update Ex_05.java

--- FichaJava/src/FichaPraticaExtraCiclos/Ex_05.java
+++ FichaJava/src/FichaPraticaExtraCiclos/Ex_05.java
@@ -1,4 +1,34 @@
 package FichaPraticaExtraCiclos;
 
+import java.util.Scanner;
 public class Ex_05 {
+
+    public static void main(String[] args) {
+
+        //declarar variaveis
+        int num,conta;
+        String linha;
+
+        //5.1
+        num=1;
+        conta=1;
+        linha = "";
+
+        for (conta=1;conta<=5;conta++){
+
+            for (num=1;num<=conta;num++){
+
+                linha += num + " ";
+
+                if (num == conta){
+
+                    linha += "\n";
+                }
+
+            }
+
+        }
+        System.out.println(linha);
+
+    }
 }
