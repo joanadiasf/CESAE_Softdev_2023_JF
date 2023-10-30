ef9a1ab207dfd2c04eb2977f5751b4469a59bd53
JoanaF <joanadiasf2001@gmail.com>
2023-10-30 10:02:14 +0000

    ex3

--- FichaJava/src/FichaPraticaExtraCiclos/Ex_03.java
+++ FichaJava/src/FichaPraticaExtraCiclos/Ex_03.java
@@ -6,7 +6,55 @@ public class Ex_03 {
 
     public static void main(String[] args) {
 
+        int num,resultado,multi;
+
+
+
+        num=1;
+        multi=1;
+
+
+        for (multi=1;multi<=10;multi++){
+
+            System.out.println("Tabuada do "+ multi + ": ");
+
+            for (num=1;num<=10;num++){
+
+                resultado = multi * num;
+
+                System.out.println(multi + "x" + num + " = " + resultado);
+            }
+        }
+
+
+
+
+
+
+
+//        System.out.println("Tabuada do " + multi + ": ");
+
+//        while (num <= 10){
+//
+//            resultado = multi * num;
+//
+//            System.out.println(multi +"x"+ num + " =" + resultado);
+//
+//            num++;
+//
+//
+//            if (num > 10){
+//                multi++;
+//
+//                System.out.println("\nTabuada do " + multi + ": ");
+//
+//            }
+//        }
 
 
     }
 }
+
+
+// for
+//      for
--- FichaJava/src/FichaPraticaExtraCiclos/Ex_04.java
+++ FichaJava/src/FichaPraticaExtraCiclos/Ex_04.java
@@ -1,4 +1,10 @@
 package FichaPraticaExtraCiclos;
 
+import java.util.Scanner;
+
 public class Ex_04 {
+
+    public static void main(String[] args) {
+
+    }
 }
