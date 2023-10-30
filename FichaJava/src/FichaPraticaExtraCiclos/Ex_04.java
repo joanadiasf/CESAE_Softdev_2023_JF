b79afc7b0061bd3138498f7356e9561070192378
JoanaF <joanadiasf2001@gmail.com>
2023-10-30 10:21:56 +0000

    dor de cabeça *

--- FichaJava/src/FichaPraticaExtraCiclos/Ex_04.java
+++ FichaJava/src/FichaPraticaExtraCiclos/Ex_04.java
@@ -7,5 +7,25 @@ public class Ex_04 {
     public static void main(String[] args) {
 
         //Declarar variáveis
+        String simbolo;
+        int conta;
+
+        conta=1;
+
+        //4.1
+        simbolo = "**********";
+
+        while (conta <=4){
+
+            System.out.println(simbolo );
+            conta++;
+
+        }
+
+        //4.2
+
+        
+
+
     }
 }
