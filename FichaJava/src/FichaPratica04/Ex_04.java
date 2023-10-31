0cbb299bdbb6fd9518b5ff242f78ac4fe1895dde
siriusjnr <joanadiasf2001@gmail.com>
2023-10-31 11:32:19 +0000

    Update Ex_04.java

--- FichaJava/src/FichaPratica04/Ex_04.java
+++ FichaJava/src/FichaPratica04/Ex_04.java
@@ -10,41 +10,31 @@ public class Ex_04 {
         Scanner input = new Scanner(System.in);
 
         //Declarar + ler variaveis
-        int valor,stop,resultado;
-        stop=0;
+        int valor,resultado;
+
+        System.out.println("Insira um número para verificar se é um número primo: \n");
+        valor = input.nextInt();
+
+        resultado = valor % 2;
 
 
+        if (resultado != 0){
 
+            resultado = valor / 3;
 
+            if (resultado != 1){
 
-        while (stop == 0){
-
-            System.out.println("Insira um número para ver se este é primo: \n");
-            valor = input.nextInt();
-
-
-            resultado= valor % 2;
-
-            if (resultado == 0){
-
-                resultado= valor % 3 ;
-
-                if (resultado != 0){
-                    System.out.println("O número inserido NÃO É PRIMO...");
-                }
-                    stop++;
+                System.out.println("Não é primo!");
             }
 
-            if ( resultado != 0){
-                System.out.println("O número é primo!!!");
-
-            }
-
-
+            System.out.println("É primo!!!");
 
         }
 
+        else {
 
+            System.out.println("Não é primo!");
+        }
 
     }
 }
