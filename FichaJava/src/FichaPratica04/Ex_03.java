e9c4ea7d71bc833180b9e317f028668be7a3590e
siriusjnr <joanadiasf2001@gmail.com>
2023-10-31 20:00:52 +0000

    Update Ex_03.java

--- FichaJava/src/FichaPratica04/Ex_03.java
+++ FichaJava/src/FichaPratica04/Ex_03.java
@@ -11,36 +11,49 @@ public class Ex_03 {
         Scanner input = new Scanner(System.in);
 
         //Declarar + ler variáveis
-        int tentativa, contador=1;
+        int numero,tentativa,contador=1,espaco;
 
-        System.out.println("Adivinhe o número! (De 0 a 100): \n");
+        System.out.println(" Jogador 1: Escolha um número! (De 0 a 100): \n");
+        numero = input.nextInt();
+
+
+        for (espaco =0; espaco <= 25; espaco++){
+
+            System.out.println();
+        }
+
+        System.out.println("Jogador 2: Adivinha o número: ");
         tentativa = input.nextInt();
 
-        System.out.println("\n");
 
-        while (tentativa >= 0 && tentativa <= 100 && tentativa !=70){
+        while (tentativa != numero){
 
-            if (tentativa > 0 && tentativa <= 69) {
+            if (tentativa < numero) {
 
                 System.out.println("Baixo");
             }
-            if (tentativa >= 71 && tentativa < 100) {
+            if (tentativa > numero) {
 
                 System.out.println("Alto");
 
-
-
             }
-        tentativa = input.nextInt();
-        contador++;
+            tentativa = input.nextInt();
+            contador++;
         }
 
-        if (tentativa==70){
+        if (tentativa==numero){
 
             System.out.println("ACERTOU!!");
             System.out.println("Número de tentativas: " + contador);
         }
+
+
+
+
+        }
+
+
+
     }
 
-}
 
