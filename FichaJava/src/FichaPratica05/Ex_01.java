c9355490f94dbf5f0684e8a19d96a8b4061f9a71
JoanaF <joanadiasf2001@gmail.com>
2023-11-03 11:39:03 +0000

    ex1

--- FichaJava/src/FichaPratica05/Ex_01.java
+++ FichaJava/src/FichaPratica05/Ex_01.java
@@ -1,4 +1,31 @@
 package FichaPratica05;
 
+import java.util.Scanner;
 public class Ex_01 {
+
+    public static void main(String[] args) {
+
+        //Scanner
+        Scanner input = new Scanner(System.in);
+
+        //Declarar variavel de vetor
+        int []vetor=new int[10];
+
+        //Ler vetor (como sabemos qts x vai ser lido, usamos um ciclo for
+        for ( int indice=0; indice < 10; indice++){
+
+            System.out.println("Insira um número no Vetor: ");
+            vetor[indice]= input.nextInt();
+
+        }
+
+        //imprimir vetores
+
+        for ( int indice=0; indice < 10; indice++){
+
+            System.out.println(vetor[indice]);
+
+        }
+
+    }
 }
