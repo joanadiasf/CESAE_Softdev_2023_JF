464860f5425e072e353d558e8b82f28f4939b294
siriusjnr <joanadiasf2001@gmail.com>
2023-11-04 22:23:40 +0000

    6

--- FichaJava/src/FichaPratica05/Ex_06.java
+++ FichaJava/src/FichaPratica05/Ex_06.java
@@ -10,7 +10,7 @@ public class Ex_06 {
 
         //Declarar variavel de vetor
         int []vetor=new int[10];
-        int aux,check=0;
+
 
 
         for ( int indice=0; indice < 10; indice++) {
@@ -18,28 +18,26 @@ public class Ex_06 {
             System.out.println("Insira um número no Vetor: ");
             vetor[indice] = input.nextInt();
 
-
         }
 
-        aux = vetor[0];
+        boolean crescente=true;
 
         for ( int indice=1; indice < 10; indice++){
 
-            if (aux > vetor[indice]){
+            if (vetor[indice] < vetor[indice -1]){
 
-                if (vetor[indice] <= aux) {
-                    check++;
-                }
-
+                crescente=false;
+                break;
 
             }
 
         }
-        if (check >0){
-            System.out.println("Não é crescente!");
-        }
-        else {
-            System.out.println("Crescente!");
+
+        if (crescente){
+
+            System.out.println("É crescente!");
+        }else {
+            System.out.println("Não é crescente...");
         }
 
 
