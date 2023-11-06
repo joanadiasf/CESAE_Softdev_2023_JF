df7a3ab390361abc1a57fbabd7a092c8588057f0
JoanaF <joanadiasf2001@gmail.com>
2023-11-06 14:27:38 +0000

    2

--- FichaJava/src/FichaPraticaExtraArraysMatrizes/Ex_02.java
+++ FichaJava/src/FichaPraticaExtraArraysMatrizes/Ex_02.java
@@ -1,7 +1,7 @@
 package FichaPraticaExtraArraysMatrizes;
 
 import java.util.Scanner;
-public class Ex_01 {
+public class Ex_02 {
 
     public static void main(String[] args) {
 
@@ -9,24 +9,25 @@ public class Ex_01 {
         Scanner input = new Scanner(System.in);
 
         //Declarar variavel de vetor
-        int [] vetor = new int[14];
+        int [] vetor = new int[10];
 
-        //Input no vetor
+        //Input
 
-        for (int l = 0; l < 14; l++){
+        for (int l = 0; l < 10; l++){
 
             System.out.print("Insira número no Vetor [" + l + "]: ");
             vetor[l] = input.nextInt();
 
         }
+        
+        System.out.println(); //espaçamento
 
-        //ler na consola
+        //Apresentar na ordem inversa
 
-        for (int l = 0; l < 14; l++){
+        for (int l = 9; l >=0; l--){
 
             System.out.println(vetor[l]);
 
         }
-
     }
 }
