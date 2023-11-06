894edeffee50127479699f2ceb34d2bd10c47ee6
JoanaF <joanadiasf2001@gmail.com>
2023-11-06 10:12:18 +0000

    11

---
+++ FichaJava/src/FichaPratica05/Ex_11.java
@@ -0,0 +1,83 @@
+package FichaPratica05;
+
+import java.util.Scanner;
+public class Ex_11 {
+
+    public static void main(String[] args) {
+
+        //Scanner
+        Scanner input = new Scanner(System.in);
+
+        //Declarar variavel de vetor - matriz
+        int [][] matriz = new int [3][3];
+        int linha, coluna,nummaior=0,nummenor=0;
+        String strg;
+
+        //string para ele imprimir mais que um nr seguido
+        strg= "";
+
+        //matriz tem linhas e colunas
+
+        for (linha = 0; linha <= 2; linha++) {
+
+
+            for (coluna = 0; coluna <= 2; coluna++) {
+
+                System.out.println("Insira um vetor: ");
+                matriz[linha][coluna]=input.nextInt();
+            }
+        }
+
+        //ver qual o numero maior
+        for (linha = 0; linha <= 2; linha++) {
+
+
+            for (coluna = 0; coluna <= 2; coluna++) {
+
+                if (nummaior < matriz[linha][coluna]){
+
+                    nummaior = matriz[linha][coluna];
+
+                }
+            }
+        }
+
+        System.out.println("\n O número maior é: " + nummaior);
+
+        //ver qual o numero menor
+        for (linha = 0; linha <= 2; linha++) {
+
+
+            for (coluna = 0; coluna <= 2; coluna++) {
+
+                if (nummenor > matriz[linha][coluna]){
+
+                    nummenor = matriz[linha][coluna];
+
+                }
+            }
+        }
+
+        System.out.println("\n O número menor é: " + nummenor);
+
+        //pra ler matriz e por nrs seguidos
+        for (linha=0; linha <=2; linha++){
+
+            for (coluna =0; coluna <=2; coluna++){
+
+                strg += matriz[linha][coluna] + " ";
+
+
+                if (coluna == 2){
+
+                    strg += "\n";
+                }
+
+            }
+        }
+
+        //imprimir FORA do for para so aparecer o que se quer
+        System.out.println("\n");
+        System.out.println(strg);
+    }
+}
