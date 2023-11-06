386ccd92a5a4f18c9679f69c4ea3340f79fbd029
JoanaF <joanadiasf2001@gmail.com>
2023-11-06 16:14:57 +0000

    aaaaaaaaaaaaaaah5

---
+++ FichaJava/src/FichaPraticaExtraArraysMatrizes/Ex_05.java
@@ -0,0 +1,63 @@
+package FichaPraticaExtraArraysMatrizes;
+
+import java.util.Scanner;
+public class Ex_05 {
+
+    public static void main(String[] args) {
+
+        //Scanner
+        Scanner input = new Scanner(System.in);
+
+        //Declarar variaveis e vetor
+        int [] vetor = new int[12];
+        int [] vetor2 = new int[12];
+        int compara;
+
+        //Input
+        for (int l = 0; l < 12; l++){
+
+            System.out.print("Insira número no Vetor [" + l + "]: ");
+            vetor[l] = input.nextInt();
+
+        }
+
+        //encontrar valores duplicados
+
+        compara = vetor[0];
+
+        //verificar linha
+
+        for ( int l = 0; l < 12; l++){
+
+
+
+            if (compara == vetor[l]){
+
+                vetor2[l] = vetor[l];
+
+            }
+            else {
+
+                vetor2[l] = vetor[l]+3;
+            }
+
+            compara = vetor[l];
+        }
+
+        for (int l = 0; l < 12; l++){
+
+            if (vetor2[l] == vetor[l]){
+
+                System.out.println("posição " + l + " " + vetor2[l]);
+            }
+
+
+
+        }
+
+
+
+    }
+}
+
+//desistoooooooooooooooooooooooooooooooooooooooooo
