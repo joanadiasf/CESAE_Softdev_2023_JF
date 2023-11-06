d0623fa010b54f33ec5da30f803977748023184d
JoanaF <joanadiasf2001@gmail.com>
2023-11-06 09:48:34 +0000

    8

---
+++ FichaJava/src/FichaPratica05/EX_08.java
@@ -0,0 +1,53 @@
+package FichaPratica05;
+
+import java.util.Scanner;
+
+public class EX_08 {
+
+    public static void main(String[] args) {
+
+        //Scanner
+        Scanner input = new Scanner(System.in);
+
+        //Declarar variavel de vetor - matriz
+        int [][] matriz = new int [3][3];
+        int linha, coluna;
+        String strg;
+
+        //string para ele imprimir mais que um nr seguido
+        strg= "";
+
+        //matriz tem linhas e colunas
+
+        for (linha = 0; linha <= 2; linha++) {
+
+
+            for (coluna = 0; coluna <= 2; coluna++) {
+
+                System.out.println("Insira um vetor: ");
+                matriz[linha][coluna]=input.nextInt();
+            }
+        }
+
+
+        //pra ler matriz e por nrs seguidos
+        for (linha=0; linha <=2; linha++){
+
+            for (coluna =0; coluna <=2; coluna++){
+
+                strg += matriz[linha][coluna] + " ";
+
+
+                if (coluna == 2){
+
+                    strg += "\n";
+                }
+
+            }
+        }
+
+        //imprimir FORA do for para so aparecer o que se quer
+        System.out.println("\n");
+        System.out.println(strg);
+    }
+}
