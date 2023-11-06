68ac3a306992b9940ac653f38bd5e83acf6c9e00
JoanaF <joanadiasf2001@gmail.com>
2023-11-06 10:47:38 +0000

    Update Ex_12.java

--- FichaJava/src/FichaPratica05/Ex_12.java
+++ FichaJava/src/FichaPratica05/Ex_12.java
@@ -26,20 +26,30 @@ public class Ex_12 {
             System.out.println("Insira um número no Vetor: ");
             vetor1[indice1]= input.nextInt();
 
+
+        }
+
+        for ( indice1=0; indice1 <= 9; indice1++){
+
             matriz[linha][0]=vetor1[indice1];
         }
 
-        System.out.println("Vetor 2");
+        System.out.println(" \nVetor 2");
 
         for (indice2=0; indice2 <= 9; indice2++){
 
             System.out.println("Insira um número no Vetor: ");
             vetor2[indice2]= input.nextInt();
 
+
+        }
+
+        for (indice2=0; indice2 <= 9; indice2++){
+
+
             matriz[linha][coluna] = vetor2[indice2];
         }
 
-          
 
         //pra ler matriz e por nrs seguidos
         for (linha=0; linha <=9; linha++){
