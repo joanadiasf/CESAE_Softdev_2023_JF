312751237a48fb95db8e2da56402fe51b82817d4
JoanaF <139019457+joanadiasf@users.noreply.github.com>
2023-11-07 14:48:14 +0000

    Update Ex_02.java

--- FichaJava/src/FichaPratica06/Ex_02.java
+++ FichaJava/src/FichaPratica06/Ex_02.java
@@ -1,4 +1,45 @@
 package FichaPratica06;
 
+import java.util.Scanner;
+
 public class Ex_02 {
+
+
+    /**
+     * Metodo que lê um inteiro positivo do utilizador
+     * @return Nr Inteiro Positivo
+     */
+    public static int lerInteiroPositivo(){
+
+        Scanner input = new Scanner(System.in);
+
+        int num;
+
+        do {
+            System.out.println("Insira um numero positivo: ");
+
+            num = input.nextInt();
+        }while(num<0);
+
+        return num;
+    }
+
+    /**
+     * Imprime nr de asteriscos = nr input
+     * @param numeroAsteriscos
+     */
+    public static void imprimirAsterisco(int numeroAsteriscos){
+
+        for (int i=0; i < numeroAsteriscos; i++){
+            System.out.print("*");
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int numInteiro = lerInteiroPositivo();
+
+        imprimirAsterisco(numInteiro);
+    }
 }
