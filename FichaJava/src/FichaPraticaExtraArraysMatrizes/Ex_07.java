ba5a1a40605d906e778163401d8b897591c56fd7
JoanaF <139019457+joanadiasf@users.noreply.github.com>
2023-11-07 12:27:37 +0000

    desespero

--- FichaJava/src/FichaPraticaExtraArraysMatrizes/Ex_07.java
+++ FichaJava/src/FichaPraticaExtraArraysMatrizes/Ex_07.java
@@ -13,42 +13,65 @@ public class Ex_07 {
         int numinsere,contador=0,retiro;
 
 
-        //Input do nr
+        //Input do nr para saber nr de elementos
         System.out.print("Quantos elementos quer inserir no Vetor? ");
         numinsere = input.nextInt();
 
-        //criação do vetor com int [variavel] para o tamanho do vetor
-        //ser o mesmo que o nr dado
+        //criação do vetor com int [variavel] para o tamanho do vetor ser o mesmo que o nr dado
         int [] vetor = new int [numinsere];
 
-        //vetor fica com tamanho do input
+        //vetor fica com tamanho do input e lê
+
         for (int l = 0; l < vetor.length; l++){
 
             System.out.print("Insira número no Vetor [" + l + "]: ");
             vetor[l] = input.nextInt();
-            contador++;
+
         }
 
         //pedir utilizador nr p/ retirar do vetor
         System.out.print("Qual o elemento que pretende remover? ");
         retiro = input.nextInt();
 
-        if (contador > 0){
 
-            numinsere= numinsere - contador;
-
-        }
-        else {
-            System.out.println("O número não está no vetor...");
-        }
-
-        vetor = new int [numinsere];
-        //vetor fica com tamanho do input
+        //contador vai contar quantas vezes o nr pretendido se repete/ou se se repete
         for (int l = 0; l < vetor.length; l++){
 
-            System.out.println(vetor[l]);
+            if (retiro == vetor[l]){
+
+                contador++;
+
+            }
 
         }
+        System.out.println("\n O nr repete-se: " + contador + "x.");
+
+
+        //informar se o nr não estiver presente no vetor
+                if (contador==0){
+                    System.out.println("O número pretendido não se encontra presente...");
+                }
+
+        //novo vetor para conter os vetores não repetidos
+        int pausa=0;
+        int [] vetor2 = new int[numinsere-contador];
+
+
+
+        for (int l = 0; l < vetor2.length; l++){
+
+            if (retiro==vetor[l]){
+
+                pausa++;
+
+            }
+            vetor2[pausa]=vetor[l];
+
+
+            System.out.println("Vetor [" + l + "]: " + vetor2[pausa]);
+
+        }
+
 
 
 
