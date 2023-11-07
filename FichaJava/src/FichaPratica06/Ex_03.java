6e195500e30d93929a74fbf937d15d19676a2019
JoanaF <139019457+joanadiasf@users.noreply.github.com>
2023-11-07 15:17:06 +0000

    3 e 4

--- FichaJava/src/FichaPratica06/Ex_03.java
+++ FichaJava/src/FichaPratica06/Ex_03.java
@@ -1,4 +1,52 @@
 package FichaPratica06;
 
 public class Ex_03 {
+
+
+    /**
+     * Avalia se um nr é par ou imar
+     * @param num - nr a ser avaliado
+     * @return true - se par || false - se impar
+     */
+    public static boolean par(int num){
+
+        if (num%2==0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    /**
+     * avalia se um nr é positivo ou negativo
+     * @param num - nr a ser avaliado
+     * @return true- positivo || false - negativo
+     */
+    public static boolean positivo (int num){
+
+            if (num>0){
+                return true;
+            }else {
+                return false;
+            }
+    }
+
+
+    /**
+     * avalia se um nr é ou não primo
+     * @param num -nr a ser avaliado
+     * @return true - primo || false-nao primo
+     */
+    public static boolean primo (int num){
+
+        for (int divisor=2; divisor <num; divisor++){
+
+            if (num%divisor==0){
+                return false;
+            }
+
+        }
+        return true;
+    }
+
 }
--- FichaJava/src/FichaPratica06/Ex_04.java
+++ FichaJava/src/FichaPratica06/Ex_04.java
@@ -1,4 +1,62 @@
 package FichaPratica06;
 
+import java.util.Scanner;
+
+//importar funcoes do ex3, o * é pra ser todas as funçoes
+import static FichaPratica06.Ex_03.*;
+
 public class Ex_04 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int numero,opcao;
+
+        System.out.println("Insira um nr:");
+        numero = input.nextInt();
+
+        do {
+            System.out.println("1");
+            System.out.println("2");
+            System.out.println("3");
+            System.out.println("4");
+            System.out.println("5");
+            System.out.println("6");
+            System.out.println("7");
+
+            System.out.println("Opção");
+            opcao = input.nextInt();
+
+
+            switch (opcao){
+
+                case 1:
+                    if (par(numero)){
+                        System.out.println("Par");
+                    }else {
+                        System.out.println("Impar");
+                    }
+                    break;
+                    
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+
+                default:
+                    System.out.println("Invalido");
+            }
+
+        }while (opcao!=7);
+
+    }
 }
