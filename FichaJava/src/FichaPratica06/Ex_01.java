e501a92a6aab716b57800bbccc838d4d3e48b75a
JoanaF <139019457+joanadiasf@users.noreply.github.com>
2023-11-07 14:35:40 +0000

    01

--- FichaJava/src/FichaPratica06/Ex_01.java
+++ FichaJava/src/FichaPratica06/Ex_01.java
@@ -1,4 +1,49 @@
 package FichaPratica06;
 
+import java.util.Scanner;
+
 public class Ex_01 {
+
+    public static void fazerBarulho(String animal){
+
+        switch (animal){
+
+            case "cao":
+                System.out.println("au au au");
+                break;
+
+            case "gato":
+                System.out.println("miau");
+                break;
+
+            case "peixe":
+                System.out.println("glub glub");
+                break;
+
+            case "vaca":
+                System.out.println("muuuu");
+                break;
+
+            case "porco":
+                System.out.println("oinc oinc");
+                break;
+
+            default:
+                System.out.println("barulho desconhecido");
+        }
+    }
+
+    //ponto de partida do programa
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String animalEscolhido;
+
+        System.out.println("Insira um animal: ");
+        animalEscolhido = input.next();
+
+        fazerBarulho(animalEscolhido);
+
+    }
 }
