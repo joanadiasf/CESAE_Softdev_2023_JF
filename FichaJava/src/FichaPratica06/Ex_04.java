602fe177aefff7aa92e28c6510829dcea891b0e5
JoanaF <139019457+joanadiasf@users.noreply.github.com>
2023-11-08 09:06:16 +0000

    finish 4

--- FichaJava/src/FichaPratica06/Ex_04.java
+++ FichaJava/src/FichaPratica06/Ex_04.java
@@ -15,20 +15,20 @@ public class Ex_04 {
         //declarar variáveis
         int numero,opcao;
 
-        System.out.println("Insira um nr:");
+        System.out.println("Insira um número:");
         numero = input.nextInt();
 
 
         do {
-            System.out.println("1");
-            System.out.println("2");
-            System.out.println("3");
-            System.out.println("4");
-            System.out.println("5");
-            System.out.println("6");
-            System.out.println("7");
+            System.out.println("\n1. Par ou Ímpar");
+            System.out.println("2. Positivo ou Negativo");
+            System.out.println("3. Primo ou Não Primo");
+            System.out.println("4. Perfeito ou Não Perfeito");
+            System.out.println("5. Triangular ou Não Triangular");
+            System.out.println("6. Trocar de Número");
+            System.out.println("7. Sair");
 
-            System.out.println("Opção");
+            System.out.println("\nOpção");
             opcao = input.nextInt();
 
 
@@ -43,20 +43,44 @@ public class Ex_04 {
                     break;
 
                 case 2:
+                    if (positivo(numero)){
+                        System.out.println("O número é: Positivo");
+                    }else {
+                        System.out.println("O número é: Negativo");
+                    }
                     break;
+
                 case 3:
+                    if (primo(numero)){
+                        System.out.println("O número é: Primo");
+                    }else {
+                        System.out.println("O número é: Não Primo");
+                    }
                     break;
+
                 case 4:
+                    if (perfeito(numero)){
+                        System.out.println("O número é: Perfeito");
+                    }else {
+                        System.out.println("O número é: Não Perfeito");
+                    }
                     break;
+
                 case 5:
+                    if (triangular(numero)){
+                        System.out.println("O número é: Triangular");
+                    }else {
+                        System.out.println("O número é: Não Triangular");
+                    }
                     break;
+
                 case 6:
-                    break;
-                case 7:
+                    System.out.println("Introduza o novo número");
+                    numero = input.nextInt();
                     break;
 
                 default:
-                    System.out.println("Invalido");
+                    System.out.println("*| Opção inválida... |*");
             }
 
         }while (opcao!=7);
