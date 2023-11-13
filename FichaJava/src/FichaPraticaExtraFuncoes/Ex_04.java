3c348463a553f6fb57beb8977e7f62ef2b035f53
JoanaF <139019457+joanadiasf@users.noreply.github.com>
2023-11-13 12:51:18 +0000

    h

---
+++ FichaJava/src/FichaPraticaExtraFuncoes/Ex_04.java
@@ -0,0 +1,39 @@
+package FichaPraticaExtraFuncoes;
+
+import java.util.Scanner;
+
+public class Ex_04 {
+
+//    public static int imprimirTabuada (int numT){
+//
+//        int resultado=0;
+//
+//        for (int i=1; i >=10; i++){
+//
+//            resultado=numT*i;
+//
+//
+//        }
+//        return resultado;
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        //Scanner
+//        Scanner input = new Scanner(System.in);
+//
+//        int numT;
+//        //input
+//        System.out.print("Que tabuada quer?");
+//        numT = input.nextInt();
+//
+//        for (int i=1; i >=10; i++){
+//
+//            System.out.println(numT + "x" + i + "=" + imprimirTabuada(numT));
+//
+//
+//        }
+//    }
+}
+//nao imprime nada
+//para depois do input no numT
