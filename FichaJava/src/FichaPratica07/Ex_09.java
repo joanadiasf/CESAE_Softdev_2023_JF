672dcd4689063cb24ba121fef8cc48c7fc80796e
JoanaF <139019457+joanadiasf@users.noreply.github.com>
2023-11-14 12:40:53 +0000

    9 aleluia

--- FichaJava/src/FichaPratica07/Ex_09.java
+++ FichaJava/src/FichaPratica07/Ex_09.java
@@ -12,38 +12,76 @@ public class Ex_09 {
         //ler ficheiro
         Scanner fileScanner = new Scanner(new File("FichaJava/Ficheiros/exercicio_09.txt"));
 
-        String linha;
-        int contaPalavras=0;
+        //1variavel - contar palavras totais | 2variavel - pra nao perder a posicao da linha anterior
+        //3variavel - para comparar com uma variavel criada à frente | 1string - guardar a palavra repetida
+        int contaPalavras=0,cont=0,rep=0;
+        String palavraRep="";
 
+
+        //ler o ficheiro e contar o nr de palavras
         while (fileScanner.hasNextLine()){
-            linha = fileScanner.nextLine();
+           String linha = fileScanner.nextLine();
 
+           String[] palavras = linha.split(" ");
 
-            String[] palavras = linha.split(" ");
+           contaPalavras+=palavras.length;
 
-            for (int i=0; i < palavras.length;i++){
-
-                contaPalavras++;
-
-            }
 
         }
-        
-//        System.out.println("Número de palavras: "+ contaPalavras);
 
+
+        //fechar o ficheiro para o scanner voltar ao inicio
+        fileScanner.close();
+
+        //voltar a abrir o ficheiro
+        Scanner fileScanner2 = new Scanner(new File("FichaJava/Ficheiros/exercicio_09.txt"));
+
+
+        //criar vetor grande para dividir as palavras por elementos do vetor
         String [] grandeVetor = new String[contaPalavras];
 
-        while (fileScanner.hasNextLine()){
-            linha = fileScanner.nextLine();
+
+        //preencher vetor
+        while (fileScanner2.hasNextLine()){
+           String linha = fileScanner2.nextLine();
             String[] palavras = linha.split(" ");
 
-            for (int i =0; i < grandeVetor.length;i++){
+            for (int i =0; i < palavras.length;i++){
 
-                grandeVetor[i] = palavras[i];
+                grandeVetor[cont] = palavras[i];
+                cont+=1;
+            }
+
+        }
+
+        //l vai procurar palavra repetida em c
+        for (int l=0; l < grandeVetor.length;l++ ){
+
+            //vai contar sempre que a palavra for igual
+            int contagemAtual=0;
+
+            for (int c=0;c < grandeVetor.length;c++){
+
+                //sempre que a palavra repetir
+                if (grandeVetor[l].equals(grandeVetor[c])){
+
+                    contagemAtual++;
+                }
+
+            }
+
+            //para guardar a palavra mais repetida
+            if (contagemAtual > rep){
+
+                rep=contagemAtual;
+                palavraRep=grandeVetor[l];
 
             }
 
         }
+
+        //print
+        System.out.println("A palavra mais repetida é: " + palavraRep);
 
 
     }
