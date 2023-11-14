ba69d15b4f010697cbc92be7663999af1521540f
JoanaF <139019457+joanadiasf@users.noreply.github.com>
2023-11-14 14:52:02 +0000

    10

--- FichaJava/src/FichaPratica07/Ex_10.java
+++ FichaJava/src/FichaPratica07/Ex_10.java
@@ -7,21 +7,23 @@ public class Ex_10 {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        //scanner de ficheiro - o path tem q estar correto
+        //scanner de ficheiro
         Scanner fileScanner = new Scanner(new File("FichaJava/Ficheiros/exercicio_10.csv"));
 
-        //String - para poder ler linha de texto
+        //ler primeira linha
         String linha= fileScanner.nextLine(); //linha cabeçalho
 
         String tipoProd,prod;
         double quantidade,valor,total=0;
 
+        //separar info por variavel para conseguir chegar ao valor
         while (fileScanner.hasNextLine()){
 
-            tipoProd = fileScanner.nextLine();
-            prod = fileScanner.nextLine();
-            quantidade = fileScanner.nextDouble();
-            valor = fileScanner.nextDouble();
+            //as "colunas" são divididas por , então: split
+            String[] palavras = linha.split(",");
+
+            //sei q os valores q quero multiplicar estao na posição 2 e 3 do vetor entao:
+            valor=(Double.parseDouble(palavras[2])*Double.parseDouble(palavras[3]));
 
             total+= valor;
 
