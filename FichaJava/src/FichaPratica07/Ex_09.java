a81880eb5b8371ba42224f816f245de19eba0f69
JoanaF <139019457+joanadiasf@users.noreply.github.com>
2023-11-14 10:21:25 +0000

    9

--- FichaJava/src/FichaPratica07/Ex_09.java
+++ FichaJava/src/FichaPratica07/Ex_09.java
@@ -1,30 +1,66 @@
 package FichaPratica07;
 
 //importar files
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.Scanner;
 public class Ex_09 {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        //scanner de ficheiro - o path tem q estar correto
+        //ler ficheiro
         Scanner fileScanner = new Scanner(new File("FichaJava/Ficheiros/exercicio_09.txt"));
 
-        //String - para poder ler linha de texto
         String linha;
-
-        //ler palavra especifica no texto
-        String palavraRepetida;
+        int contaPalavras=0;
 
         while (fileScanner.hasNextLine()){
             linha = fileScanner.nextLine();
-            System.out.println(linha);
+
+
+            String[] palavras = linha.split(" ");
+
+            for (int i=0; i < palavras.length;i++){
+
+                contaPalavras++;
+
+            }
+
         }
+        System.out.println("Número de palavras: "+ contaPalavras);
+
+        String [] grandeVetor = new String[contaPalavras];
+
+        while (fileScanner.hasNextLine()){
+            linha = fileScanner.nextLine();
+
+            String[] palavras = linha.split(" ");
+
+            for (int i =0; i < grandeVetor.length;i++){
+
+                
+
+            }
+
+        }
+
 
     }
 }
 
 //ler ficheiro
 //imprimir palavra mais frequente
+
+
+//1 contar nr palavras
+//2criar vetor c este tamanho
+//3pegar numa linha
+//4limpar a linha
+//5split
+//6passar cada posição para o vetor
+//voltar passo 3
+
+//7 analisar vetor grande
+
+//contador pra nao perder a posicao da linha anterior
