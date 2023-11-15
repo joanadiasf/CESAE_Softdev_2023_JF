0cb55ae3727472f31b0e2f5f5e8743f1bd53400f
siriusjnr <joanadiasf2001@gmail.com>
2023-11-15 10:26:53 +0000

    pe

--- FichaJava/src/PE_joanaFernandes/GameStart_JoanaFernandes.java
+++ FichaJava/src/PE_joanaFernandes/GameStart_JoanaFernandes.java
@@ -9,9 +9,45 @@ public class GameStart_JoanaFernandes {
 
     public static void main(String[] args) throws FileNotFoundException {
 
+        //ler ficheiro + ler input do utilizador
         Scanner fileScanner = new Scanner(new File("src/PE_joanaFernandes/GameStart_V2.csv"));
+        Scanner input = new Scanner(System.in);
 
-        
+
+        String menuAdmClient;
+
+        //espaçamento
+        System.out.println();
+
+        System.out.println("*****| BEM - VINDO |*****");
+
+
+        //menu1 - admin ou client
+
+        do {
+
+            System.out.print("Tipo de Utilizador ( ADMIN || CLIENT): ");
+            menuAdmClient = input.next();
+
+            switch (menuAdmClient){
+
+                case "ADMIN":
+                    //funçao menu admin
+                    System.out.println("admin");
+                    break;
+
+                case "CLIENTE":
+                    //funçao menu client
+                    System.out.println("client");
+                    break;
+
+                default:
+                    System.out.println("------ Opção inválida... ------");
+                    System.out.println("------ Tente novamente ------");
+            }
+        }while (!menuAdmClient.equals("ADMIN") && !menuAdmClient.equals("CLIENTE"));
+
+
     }
 
 }
--- FichaJava/src/PE_joanaFernandes/funcoes.java
+++ FichaJava/src/PE_joanaFernandes/funcoes.java
@@ -1,4 +1,14 @@
 package PE_joanaFernandes;
 
 public class funcoes {
+
+    public static void menuAdmin(){
+
+
+    }
+
+    public static void menuClient(){
+
+
+    }
 }
