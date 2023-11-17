9e48e7c5ddebade2ba5e405a371090a35500e25d
JoanaF <139019457+joanadiasf@users.noreply.github.com>
2023-11-17 12:41:53 +0000

    9

---
+++ Programação/FichaPratica09/src/Ex_06/Calculadora.java
@@ -0,0 +1,10 @@
+package Ex_06;
+
+public class Calculadora {
+
+
+
+}
+
+//metodo par adicao,subtracao,multiplicacao,divisao
+//no main realizar pelo menos 1x cada operação
---
+++ Programação/FichaPratica09/src/Ex_06/Main.java
@@ -0,0 +1,9 @@
+package Ex_06;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Instanciar numeros da calculadora
+    }
+}
