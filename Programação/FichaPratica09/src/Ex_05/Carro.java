edc0196db578a95c6acf34c3ed36fd457eecc650
JoanaF <139019457+joanadiasf@users.noreply.github.com>
2023-11-17 12:29:12 +0000

    carrinho

---
+++ Programação/FichaPratica09/src/Ex_05/Carro.java
@@ -0,0 +1,22 @@
+package Ex_05;
+
+public class Carro {
+
+    private String modelo;
+    private String cor;
+    private String nrPortas;
+    private String nrPassageiros;
+
+    public void ligado(){
+
+        System.out.println("\nO carro está ligado!");
+        System.out.println();
+        System.out.println("     _____     ");
+        System.out.println("   /    | \\   ");
+        System.out.println("|           ---|");
+        System.out.println("----()----()---|");
+    }
+
+}
+//criar metodo na classe Carro chamado "ligar" que imprima "O carro está ligado"
+//criar objeto da classe Carro que invoque o método ligar
---
+++ Programação/FichaPratica09/src/Ex_05/Main.java
@@ -0,0 +1,12 @@
+package Ex_05;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Carro carro1 = new Carro();
+
+        carro1.ligado();
+
+    }
+}
