8a295d37d335e4eee740e2affd4d47a41fdb4476
JoanaF <139019457+joanadiasf@users.noreply.github.com>
2023-11-20 12:22:42 +0000

    fp10

---
+++ Programação/FichaPratica10/src/Ex_01/Carro.java
@@ -0,0 +1,19 @@
+package Ex_01;
+
+public class Carro {
+
+    private String modelo;
+    private String cor;
+    private String nrPortas;
+    private String nrPassageiros;
+
+    public void ligado(){
+
+        System.out.println("\nO carro está ligado!");
+        System.out.println();
+        System.out.println("     _____     ");
+        System.out.println("   /    | \\   ");
+        System.out.println("|           ---|");
+        System.out.println("----()----()---|");
+    }
+}
