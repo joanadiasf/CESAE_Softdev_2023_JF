9d97156f8c968315ce78e512f3ac14832b62fb2f
siriusjnr <joanadiasf2001@gmail.com>
2023-12-05 09:38:23 +0000

    3 14

---
+++ Programação/FichaPratica14/src/Ex_03/Pessoa.java
@@ -0,0 +1,40 @@
+package Ex_03;
+
+public abstract class Pessoa {
+
+    private String nome;
+    private int anoNascimento;
+    private String email;
+    private String telemovel;
+
+    /**
+     * Construtor
+     * @param nome - nome
+     * @param anoNascimento - ano de nascimento
+     * @param email - email
+     * @param telemovel - numero de telemovel
+     */
+    public Pessoa(String nome, int anoNascimento, String email, String telemovel) {
+        this.nome = nome;
+        this.anoNascimento = anoNascimento;
+        this.email = email;
+        this.telemovel = telemovel;
+    }
+
+    //métodos abstratos
+    public abstract void obterFuncao();
+    public abstract void imprimirHorario();
+
+    /**
+     * Método exibir detalhes
+     */
+    public void exibirDetalhes(){
+
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Ano Nascimento: " + this.anoNascimento);
+        System.out.println("Email: " + this.email);
+        System.out.println("Telemóvel: " + this.telemovel);
+        
+    }
+
+}
