01a241b3ccdf06db3dfbf83ecbd8e44d2ffaf120
JoanaF <139019457+joanadiasf@users.noreply.github.com>
2023-12-11 13:05:51 +0000

    1

--- Programação/FactoryMethod/src/Ex_01/Main.java
+++ Programação/FactoryMethod/src/Ex_01/Main.java
@@ -1,9 +1,24 @@
 package Ex_01;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Tipo de Produto (Livro,Eletronico,Roupa)");
+        String tipo = input.next();
+
+        System.out.println("Nome");
+        String nome = input.next();
+
+        System.out.println("Preço");
+        double preco = input.nextDouble();
+
+        Produto produtoUser = ProductFactory.criarProduto(tipo,nome,preco);
+
+        produtoUser.mostrar();
     }
 }
