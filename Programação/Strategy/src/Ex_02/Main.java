7ffa1f2467d6cfebc9457db95b2a0c851bca7a1c
siriusjnr <joanadiasf2001@gmail.com>
2023-12-11 19:20:05 +0000

    Update Main.java

--- Programação/Strategy/src/Ex_02/Main.java
+++ Programação/Strategy/src/Ex_02/Main.java
@@ -6,14 +6,14 @@ public class Main {
 
         PaymentProcessor paymentProcessor = new PaymentProcessor(new NoDiscountStrategy());
         double total= paymentProcessor.discountCalc(100.0);
-        System.out.println("Tota sem desconto: " + total);
+        System.out.println("Total sem desconto: " + total);
 
         paymentProcessor = new PaymentProcessor(new FlatDiscountStrategy());//10.0
         total= paymentProcessor.discountCalc(100);
-        System.out.println("Tota com desconto fixo: " + total);
+        System.out.println("Total com desconto fixo: " + total);
 
         paymentProcessor = new PaymentProcessor(new PercentageDiscountStrategy()); //0.2
         total= paymentProcessor.discountCalc(100);
-        System.out.println("Tota com desconto percentual: " + total);
+        System.out.println("Total com desconto percentual: " + total);
     }
 }
