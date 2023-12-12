e54e020c47c278b862d07ecfa6985031183f4b58
JoanaF <139019457+joanadiasf@users.noreply.github.com>
2023-12-12 12:45:19 +0000

    mvc

---
+++ Programação/MVC/src/Model_Repository/VendasRepositorio.java
@@ -0,0 +1,19 @@
+package Model_Repository;
+
+import Domain.Venda;
+import Tools.CSVVendasReader;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class VendasRepositorio {
+
+    private ArrayList <Venda> vendaArrayList;
+
+    public VendasRepositorio(String filePath) throws FileNotFoundException {
+        CSVVendasReader csvVendasReader = new CSVVendasReader(filePath);
+        this.vendaArrayList= csvVendasReader.readCSVToRepository();
+    }
+
+
+}
