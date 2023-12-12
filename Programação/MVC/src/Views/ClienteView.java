94713e4c68f4998175b66e52ca97d7078004baab
JoanaF <139019457+joanadiasf@users.noreply.github.com>
2023-12-12 12:54:40 +0000

    clientview interface

---
+++ Programação/MVC/src/Views/ClienteView.java
@@ -0,0 +1,18 @@
+package Views;
+
+public class ClienteView {
+
+
+    public void menuCliente(){
+
+        System.out.println("\n********** M E N U **********");
+        System.out.println("1- Consultar Produtos Disponíveis");
+        System.out.println("2- Consultar Produtos");
+        System.out.println("3- Consultar Produto Mais Barato");
+        System.out.println("4- Consultar Produto Mais Caro");
+        System.out.println("0- Sair");
+
+        System.out.println("\nSelecione uma opção ******");
+        
+    }
+}
