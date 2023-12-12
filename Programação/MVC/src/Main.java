a3d2d68a4df00b2f8fc65cf2d0aa20b8667b6fd2
JoanaF <139019457+joanadiasf@users.noreply.github.com>
2023-12-12 13:01:52 +0000

    mvc

---
+++ Programação/MVC/src/Main.java
@@ -0,0 +1,10 @@
+import Views.ClienteView;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ClienteView cv = new ClienteView();
+
+        cv.menuCliente();
+    }
+}
--- Programação/MVC/src/Views/ClienteView.java
+++ Programação/MVC/src/Views/ClienteView.java
@@ -1,18 +1,45 @@
 package Views;
 
+import java.util.Scanner;
+
 public class ClienteView {
 
 
     public void menuCliente(){
 
-        System.out.println("\n********** M E N U **********");
-        System.out.println("1- Consultar Produtos Disponíveis");
-        System.out.println("2- Consultar Produtos");
-        System.out.println("3- Consultar Produto Mais Barato");
-        System.out.println("4- Consultar Produto Mais Caro");
-        System.out.println("0- Sair");
+        Scanner input = new Scanner(System.in);
+        int opcao=0;
+
+        System.out.println("\n********** M E N U ******************");
+        System.out.println("* 1- Consultar Produtos Disponíveis *");
+        System.out.println("* 2- Consultar Produtos             *");
+        System.out.println("* 3- Consultar Produto Mais Barato  *");
+        System.out.println("* 4- Consultar Produto Mais Caro    *");
+        System.out.println("* 0- Sair                           *");
+        System.out.println("*************************************");
 
         System.out.println("\nSelecione uma opção ******");
-        
+        opcao = input.nextInt();
+
+
+        switch (opcao){
+
+            case 1:
+                System.out.println("");
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            case 0:
+                break;
+
+        }
     }
 }
