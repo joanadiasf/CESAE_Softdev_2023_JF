19247dc7f1062ff1013e77e2dca94ab15dbf09a9
siriusjnr <joanadiasf2001@gmail.com>
2024-01-05 14:57:55 +0000

    ex update

--- Ficha_JUnit/src/test/java/Ex_01/CalculatorTest.java
+++ Ficha_JUnit/src/test/java/Ex_01/CalculatorTest.java
@@ -18,32 +18,46 @@ public class CalculatorTest {
     @Test
     public void testAdd(){
 
-        assertEquals(5,calculator.add(5,0));
-        //fazer mais
+        assertEquals(10,calculator.add(7,3));
+        assertEquals(15,calculator.add(15,0));
+        assertEquals(60,calculator.add(100,-40));
+        assertEquals(-30,calculator.add(10,-40));
+        assertEquals(0,calculator.add(40,-40));
+        assertEquals(0,calculator.add(0,0));
+
     }
 
     @Test
     public void testSubtract(){
 
         assertEquals(50,calculator.subtract(110,60));
-        //fazer mais
+        assertEquals(-170,calculator.subtract(-110,60));
+        assertEquals(0,calculator.subtract(5,5));
+        assertEquals(10,calculator.subtract(5,-5));
+
     }
 
     @Test
     public void testMultiply(){
 
         assertEquals(20,calculator.multiply(10,2));
-        //fazer mais
+        assertEquals(0,calculator.multiply(10,0));
+        assertEquals(10,calculator.multiply(10,1));
+
     }
 
     @Test
     public void testDivide(){
 
-        assertEquals(10,calculator.divide(20,2));
+        assertEquals(10,calculator.divide(100,10));
     }
 
     @Test
     public void testDividePerZero(){
+
+        assertThrows(IllegalArgumentException.class,() -> {
+            calculator.divide(10,0);
+        });
 
         assertThrows(IllegalArgumentException.class,() -> {
             calculator.divide(0,0);
