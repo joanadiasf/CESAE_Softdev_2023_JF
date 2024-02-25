acd15caed2f10fa812e05c21d69da472d860a83a
siriusjnr <joanadiasf2001@gmail.com>
2024-02-25 14:19:48 +0000

    home filler

--- PHP/Projeto_Bandas/database/migrations/2024_02_24_190418_add_user_type_to_user.php
+++ PHP/Projeto_Bandas/database/migrations/2024_02_24_190418_add_user_type_to_user.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('user_type')->after('email')->default(0);
+            $table->integer('user_type')->after('email')->default(2);
             $table->string('photo')->after('name')->nullable();
 
         });
--- PHP/Projeto_Bandas/resources/views/main/home.blade.php
+++ PHP/Projeto_Bandas/resources/views/main/home.blade.php
@@ -4,12 +4,25 @@
 <link rel="stylesheet" href="/css/master.css">
 
 
-<h3>HOMEEEEEEEE</h3>
+<h3>HOME</h3>
 
 <br>
 
+
+<h1>Unleash the Sound Waves &#x1F918;</h1>
+
+<p>Join us on an epic musical journey filled with raw energy and passion. From heart-pounding rhythms to soul-stirring
+  lyrics, our songs transcend boundaries and ignite spirits. Feel the adrenaline rush as guitar riffs electrify the air,
+  while every beat resonates with your soul. Embrace the essence of rock as we unleash the power of sound, uniting
+  hearts and minds in a symphony of emotions. Get ready to lose yourself in the magic of our music, where every chord
+  strummed and every lyric sung speaks volumes. Let's rock the world together!</p>
+
+
+
+
 @auth
 @if (Auth::user()->user_type == 1)
+<br>
 <div class="container text-center">
   <div class="row">
     <div class="col">
